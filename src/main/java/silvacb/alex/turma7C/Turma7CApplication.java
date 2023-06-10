e01fea9bf489bd26a656910c4df6b7c1879f75ea
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-06-09 22:23:54 -0300

    Projeto criado

---
+++ src/main/java/silvacb/alex/turma7C/Turma7CApplication.java
@@ -0,0 +1,13 @@
+package silvacb.alex.turma7C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Turma7CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Turma7CApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silvacb/alex/turma7C/Turma7CApplicationTests.java
@@ -0,0 +1,13 @@
+package silvacb.alex.turma7C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Turma7CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
